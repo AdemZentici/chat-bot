a3406e2b10dc61c731ff41e1bc4251e5db6502db
komeil-yeganeh <komeilyeganeh261@gmail.com>
2026-01-01 01:32:31 +0330

    Chat service and conversation repository added to project/server

---
+++ server/services/chat.service.ts
@@ -0,0 +1,27 @@
+import OpenAI from "openai";
+import { conversationRepository } from "../repositories/conversation.repository";
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export class ChatService {
+  async sendMessage(
+    prompt: string,
+    conversationId: string
+  ): Promise<{ id: string; message: string }> {
+    const responses = await client.responses.create({
+      model: "gpt-4o-mini",
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 200,
+      previous_response_id:
+        conversationRepository.getLastResponseId(conversationId),
+    });
+    conversationRepository.setLastResponseId(conversationId, responses.id);
+    return {
+      id: responses.id,
+      message: responses.output_text,
+    };
+  }
+}
